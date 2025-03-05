69fc0421163f566cb811b70ac0d855e951f3a688
Anastasiya_Mianko <anastasiya_mianko@epam.com>
2025-03-05 15:04:41 +0300

    fix: replace legacy code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,5 @@ import '../index.css';
 import { wrapper } from '../store/wrapper';
 import { MyApp } from '../components/app';
 
-const WrappedApp = wrapper.withRedux(MyApp);
+const WrappedApp = wrapper.useWrappedStore(MyApp);
 export default WrappedApp;
